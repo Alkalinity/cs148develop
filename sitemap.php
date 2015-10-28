593ed705a9349545cbf0c70ac3d59d5d67f6c797
Samuel Pakulski <spakulsk@uvm.edu>
2015-10-28 13:13:37 -0400

    Fixed sitemap

--- sitemap.php
+++ sitemap.php
@@ -13,7 +13,7 @@
             <li>Assignment 2 - <a href="assignment2.0/select.php">The Select Statement</a> <a href="assignment2.0/queryOne.php">Query One</a> <a href="assignment2.0/queryTwo.php">Query Two</a> <a href="assignment2.0/queryThree.php">Query Three</a> <a href="assignment2.0/queryFour.php">Query Four</a> <a href="assignment2.0/queryFive.php">Query Five</a> <a href="assignment2.0/querySix.php">Query Six</a> <a href="assignment2.0/querySeven.php">Query Seven</a> <a href="assignment2.0/queryEight.php">Query Eight</a> <a href="assignment2.0/queryNine.php">Query Nine</a> <a href="assignment2.0/queryTen.php">Query Ten</a> </li> 
             <li>Assignment 3 - <a href="assignment3.0/select.php">The JOIN Statement</a> <a href="assignment3.0/queryOne.php">Query One</a> <a href="assignment3.0/queryTwo.php">Query Two</a> <a href="assignment3.0/queryThree.php">Query Three</a> <a href="assignment3.0/queryFour.php">Query Four</a> <a href="assignment3.0/queryFive.php">Query Five</a> <a href="assignment3.0/querySix.php">Query Six</a> <a href="assignment3.0/querySeven.php">Query Seven</a> <a href="assignment3.0/queryEight.php">Query Eight</a></li> 
             <li>Misc - <a href="misc/select.php">Friday Assignment</a> <a href="misc/friday.php">Friday</a></li>
-            <li>Advising - <a href="advising/tables.php">Advising Assignment</a> <a href="advising/schema/FourYearPlanERDiagram.pdf">ER Diagram</a> <a href="advising/schema/Schema.pdf">Schema</a></li>
+            <li>Advising - <a href="advising/tables.php">Advising Assignment</a> <a href="advising/Schema/FourYearPlanERDiagram.pdf">ER Diagram</a> <a href="advising/Schema/Schema.pdf">Schema</a></li>
         </ol>
         </article>
     </body>
